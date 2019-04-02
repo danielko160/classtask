ab9f463791da9a2024fe0f57372fc0b72877b9dc
danielko160 <danielko160@gmail.com>
2019-04-02 19:05:53 +0300

    This is the first file to open, hello output

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main (String[] args) {
+
+        System.out.println("Hello!");
+        System.out.println("This is a test code in java. ");
+        System.out.println("Amazing!");
+
+    }
+
+}
+
+
